af21acfa44a9b133c772ceeeeea8c53139366450
Jonas Bøttzauw Pedersen <jonas@bpedersen.xyz>
2023-04-25 15:25:55 +0200

    Skeleton for AGM Contraction tests

---
+++ agm_contraction.py
@@ -0,0 +1,26 @@
+from main import *
+import sympy
+
+class TestAGMContration:
+    def test_test(self):
+        assert 1+1 == 4
+    def test_closure(self):
+        p,q = sympy.symbols('p q')
+        expressions = {(p >> r), (~r >> q) & (q >> ~r)}
+        expressions
+
+        phi = 
+
+    def test_success(self):
+
+    def test_inclusion(self):
+
+    def test_vacuity(self):
+
+    def test_extensionality(self):
+
+    def test_recovery(self):
+
+    def test_conj_inclusion(self):
+    
+    def test_conj_overlap(self):
